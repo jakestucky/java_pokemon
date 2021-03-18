e4e7f50bc639e12d4e918754506b25bb515b27aa
jake.stucky@daugherty.com <JFS1031@daugherty.com>
2021-03-18 13:44:10 -0500

    first commit with todo

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -3,7 +3,16 @@ package com.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//TODO build pokemon constructor /class
+//TODO populate the DB
+//TODO Build Interface
+//TODO Build Business layer
+//TODO GET: /pokemon/{id} Returns pokemon
+//TODO PUT: /pokemon/{id} Updates pokemon
+//TODO POST: /pokemon Add pokemon
+//TODO GET: /pokemon Returns list of pokemon
+//TODO POST: /pokemon/battle (takes in 2 pokemon) Returns the winning pokemon
+//TODO CHALLENGE: updates history table
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
